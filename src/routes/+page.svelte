b4a603158cfb8c1471b2f66668f5ca2bbe536541
NAD LAB <61890796+aimforbigfoot@users.noreply.github.com>
2023-07-23 18:11:35 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<p class="text-4xl">Ontario Game Dev Verse Game Jam</p>
+		<p> <a  href="/joinlist"> Want to stay in the loop?</a> </p>
+	</div>
+</div>
---
+++ src/routes/joinlist/+page.svelte
@@ -0,0 +1,5 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center pt-12">
+	<p>HELLO, I'll put email signing up stuff here in a bit</p>
+</div>
