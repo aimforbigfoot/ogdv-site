e8aa5ee11e213d631be22b72e6c056a2ec0adff3
NAD LAB <61890796+aimforbigfoot@users.noreply.github.com>
2023-09-03 15:12:39 -0400

    pre firebase commit, nothing changed

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -49,7 +49,6 @@ declare module '$env/static/private' {
 	export const JAVA_HOME: string;
 	export const LANG: string;
 	export const LOCALAPPDATA: string;
-	export const LOCAL_GIT_DIRECTORY: string;
 	export const LOGONSERVER: string;
 	export const NODE: string;
 	export const NODE_ENV: string;
@@ -168,7 +167,6 @@ declare module '$env/dynamic/private' {
 		JAVA_HOME: string;
 		LANG: string;
 		LOCALAPPDATA: string;
-		LOCAL_GIT_DIRECTORY: string;
 		LOGONSERVER: string;
 		NODE: string;
 		NODE_ENV: string;
