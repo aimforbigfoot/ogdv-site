6d85eecbe01399c463e49beb738d53663efd7661
JUB_Yoush <yoush1337@gmail.com>
2023-11-20 21:24:07 -0500

    ui lib not working :(

--- src/comps/AboutUs.svelte
+++ src/comps/AboutUs.svelte
@@ -2,24 +2,20 @@
 </script>
 
 <div class="boxAround w-80 ml-auto mr-auto pa4" id="aboutUs">
-  <h1 class="h1 tl white">About Us!</h1>
-
   <div class="aboutUsBox ff7 pa4 mt4 text-lg">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent est lorem,
-    malesuada quis euismod quis, ornare in mauris. Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis
-    parturient montes, nascetur ridiculus mus. Duis risus est, dignissim sit
-    amet nisi eu, venenatis lobortis ex. Sed ultrices vestibulum ultricies. In
-    hac habitasse platea dictumst. Nunc ac varius risus, at efficitur mauris.
-    Cras non massa diam. Ut finibus erat tortor, vel congue risus auctor a.
-    Fusce ac consectetur velit, vitae convallis libero. Donec nec justo eget
-    magna accumsan vulputate et ac odio. Quisque vulputate lorem libero, ac
-    vestibulum odio porta commodo. Pellentesque est ante, tincidunt ac porttitor
-    eu, efficitur non purus. Nullam sodales lacus eu posuere sodales. Integer
-    egestas, quam vel facilisis mollis, elit magna ornare nibh, sit amet ornare
-    mauris dolor at lectus. Curabitur vitae lorem quis risus vehicula dignissim.
-    Ut convallis pulvinar molestie. Etiam accumsan dui et velit pulvinar, in
-    scelerisque mauris consectetur. Duis a sagittis nunc. Curabitur turpis elit,
-    pulvinar nec commodo vel, ullamcorper sit amet est.
+    <h1 class="text-left">ABOUT US:</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent est
+      lorem, malesuada quis euismod quis, ornare in mauris. Lorem ipsum dolor
+      sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Duis risus est,
+      dignissim sit amet nisi eu, venenatis lobortis ex. Sed ultrices vestibulum
+      ultricies. In hac habitasse platea dictumst. Nunc ac varius risus, at
+      efficitur mauris. Cras non massa diam. Ut finibus erat tortor, vel congue
+      risus auctor a. Fusce ac consectetur velit, vitae convallis libero. Donec
+      nec justo eget magna accumsan vulputate et ac odio. Quisque vulputate
+      lorem libero, ac vestibulum odio porta commodo. Pellentesque est ante,
+      tincidunt ac porttitor eu, efficitur non purus.
+    </p>
   </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,48 @@
 <script>
-    import { scrollRef, scrollTo } from 'svelte-scrolling';
-    import AboutUs from '../comps/AboutUs.svelte';
-    import EmailSignUp from '../comps/EmailSignUp.svelte';
-    import EventInfo from '../comps/EventInfo.svelte';
-    import IntroPage from '../comps/IntroPage.svelte';
-    import SiteFooter from '../comps/SiteFooter.svelte';
+  import { scrollRef, scrollTo } from "svelte-scrolling";
+  import Faq from "../comps/Faq.svelte";
+  import AboutUs from "../comps/AboutUs.svelte";
+  import EmailSignUp from "../comps/EmailSignUp.svelte";
+  import EventInfo from "../comps/EventInfo.svelte";
+  import IntroPage from "../comps/IntroPage.svelte";
+  import SiteFooter from "../comps/SiteFooter.svelte";
 </script>
 
 <svelte:head>
-    <title>Home</title>
-    <meta name="description" content="Svelte demo app" />
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <main class="w-100-l rainbow-box">
-    <IntroPage />
-    <hr />
-    <div class="div" use:scrollRef={'about'}>
-        <AboutUs />
-    </div>
-    <hr />
-    <div class="eventInfoDiv" use:scrollRef={'eventInfo'}>
-        <EventInfo />
-    </div>
-    <hr />
-    <div class="signUP" use:scrollRef={"signUp"}>
-        <EmailSignUp />
-    </div>
+  <IntroPage />
+  <hr />
+  <div class="div" use:scrollRef={"about"}>
+    <AboutUs />
+  </div>
+  <hr />
+  <div class="eventInfoDiv" use:scrollRef={"eventInfo"}>
+    <EventInfo />
+  </div>
+  <hr />
+  <div class="signUP" use:scrollRef={"signUp"}>
+    <EmailSignUp />
+  </div>
+  <div>
+    <Faq />
+  </div>
 
-    <hr />
+  <hr />
 </main>
 <SiteFooter />
 
 <style>
-    .title {
-        flex-grow: 1;
-    }
-    .logo-image {
-        flex-grow: 1;
-    }
-    .navbar {
-        flex-grow: 1;
-    }
+  .title {
+    flex-grow: 1;
+  }
+  .logo-image {
+    flex-grow: 1;
+  }
+  .navbar {
+    flex-grow: 1;
+  }
 </style>
