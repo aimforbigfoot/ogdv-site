a4c3ef2502efedbb5b59d42015305b8ba2a57ecb
JUB_Yoush <yoush1337@gmail.com>
2023-11-20 20:44:10 -0500

    new textboxes

--- src/comps/IntroPage.svelte
+++ src/comps/IntroPage.svelte
@@ -1,32 +1,36 @@
 <script>
-    import { scrollTo } from 'svelte-scrolling';
-    import logo from '../lib/images/logo.png';
+  import { scrollTo } from "svelte-scrolling";
+  import logo from "../lib/images/logo.png";
 </script>
 
 <div class="flex justify-center">
-    <div
-        class="flex flex-column flex-row-ns justify-between items-center w-90-ns mw7-l mt5 landing-title mr4 pr4"
-    >
-        <div class="mr4">
-            <div
-                class="ml2 mr2 b tc br b--white border w-100-l flex flex-column justify-around h-100"
-            >
-                <a class="pv3 mv3 f3" use:scrollTo={'about'}>ABOUT US </a>
-                <a class="pv3 mv3 f3" use:scrollTo={'eventInfo'}>EVENT INFO</a>
-                <a class=" mv3 f3" use:scrollTo={'signUp'}>SIGN UP</a>
-            </div>
-        </div>
-        <div class="mw5">
-            <img src={logo} alt="logo" class="logo-image" />
-        </div>
-        <div
-            class=" tl di pl4 title white white-text-with-red-shadow pulsating-shadow"
-        >
-            <h1 class="di tl f3 pa0 ma0">ONTARIO</h1>
-            <br />
-            <h1 class="di tl i f1 pa0 ma0">GAMEVERSE</h1>
-        <br />
-            <h1 class="tl bigBlitzText">BLITZ</h1>
-        </div>
+  <div
+    class="flex flex-col md:flex-row justify-between items-center w-90 md:w-full lg:max-w-screen-lg mt-5 md:mt-0 landing-title mr-4 md:pr-4"
+  >
+    <div class="mr-4">
+      <div
+        class="ml2 mr2 b tc br b--white border w-100-l flex flex-row lg:flex-col justify-around h-100"
+      >
+        <a class="pv3 mv3 f3" use:scrollTo={"about"}>ABOUT US </a>
+        <a class="pv3 mv3 f3" use:scrollTo={"eventInfo"}>EVENT INFO</a>
+        <a class=" mv3 f3" use:scrollTo={"signUp"}>SIGN UP</a>
+      </div>
     </div>
+    <div class="max-w-5xl">
+      <img src={logo} alt="logo" class="logo-image" />
+    </div>
+    <div
+      class="text-left inline-block pl-16 text-4xl text-white pulsating-shadow"
+    >
+      <h1 class="inline text-left text-3xl p-0 m-0">ONTARIO</h1>
+      <br />
+
+      <h1 class="inline text-left italic text-5xl p-0 m-0">GAMEVERSE</h1>
+      <br />
+
+      <h1 class="text-left text-9xl font-extrabold tracking-wide bigBlitzText">
+        BLITZ
+      </h1>
+    </div>
+  </div>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
-import "../css/styles.css";
+  import "../app.pcss";
+  import "../css/styles.css";
   import "../css/tachyons.css";
+  import "../css/ff7.css";
 </script>
 
 <div class="app bg-black">
   <main>
     <slot />
   </main>
-
 </div>
